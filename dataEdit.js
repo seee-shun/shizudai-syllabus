e60d2b8f6b564749de1529f613481160ce229c46
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2022-04-01 18:25:47 +0900

    fix: 2022-syllabus

--- dataEdit.js
+++ dataEdit.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
+const fs = require("fs");
 // var mainObj = {};
-let mainObj = JSON.parse(fs.readFileSync('./data2021.json', 'utf8'));
+let mainObj = JSON.parse(fs.readFileSync("./data2022.json", "utf8"));
 for (let key in mainObj) {
   mainObj[key].bigtitle = mainObj[key].bigtitle.trim();
   mainObj[key].category = mainObj[key].category.trim();
@@ -12,15 +12,15 @@ for (let key in mainObj) {
   mainObj[key].department = mainObj[key].department.trim();
   mainObj[key].term = mainObj[key].term.trim();
   mainObj[key].time = mainObj[key].time.trim();
-  if (mainObj[key].teacher == '') {
-    mainObj[key].teacher = '教員情報なし';
+  if (mainObj[key].teacher == "") {
+    mainObj[key].teacher = "教員情報なし";
   }
   mainObj[key].listTitle =
-    mainObj[key].title + '（' + mainObj[key].teacher + '）';
+    mainObj[key].title + "（" + mainObj[key].teacher + "）";
   mainObj[key].listTitle = mainObj[key].listTitle.trim();
 }
 
-fs.writeFile('data.json', JSON.stringify(mainObj), (err) => {
+fs.writeFile("data.json", JSON.stringify(mainObj), (err) => {
   if (err) throw err;
-  console.log('done');
+  console.log("done");
 });
